0092c7829796f3a64f5458bad7ad7599ecbe0028
arjow <arjow@protonmail.com>
2022-10-13 13:36:30 +0200

    add docstrings to stappenmotor.py.

--- TODO.md
+++ TODO.md
@@ -3,18 +3,22 @@ applications for pico and a script to run.
 
 ### To Do
 
-- [ ] wifi docstring  
+- [ ] change and chekc  machine.Timer functions  
+- [ ] dht11 measure with timestamp  
+- [ ] rewrite stappenmotor to class  
 - [ ] create README file with documentation  
 - [ ] rewriting micropython_sonar  
 - [ ] sonar radar  
 
 ### in Progress
 
+- [ ] stappenmotor docstring  
 
 ### check code?
 
 - [ ] dht_sensor docstring  
 - [ ] weather docstring  
+- [ ] wifi docstring  
 
 ### Completed Column ✓
 
--- api/stappenmotor.py
+++ api/stappenmotor.py
@@ -10,26 +10,97 @@ from time import sleep_ms
 
 m_pins = [Pin(i, Pin.OUT) for i in range(6, 10)]
 
+
 def step(time, dirr=0):
+    '''Rotate 1 fullstep.
+
+    Parameters
+    ----------
+    sleep: int
+        delay in ms
+    dirr: bool (o or 1)
+        Dirrection of step. Left or right rotation.
+    '''
     for i in range(0+4*dirr, 4-4*dirr, 1-dirr*2):
-        m_pins[(0+i)%4].on(); m_pins[(1+i)%4].off(); m_pins[(2+i)%4].off(); m_pins[(3+i)%4].off()
+        m_pins[(0+i) % 4].on()
+        m_pins[(1+i) % 4].off()
+        m_pins[(2+i) % 4].off()
+        m_pins[(3+i) % 4].off()
         sleep_ms(time)
+
+
+def steps(steps, time, dirr=0):
+    '''Rotate multiple steps.
+
+    Parameters
+    ----------
+    steps: int
+        number of steps
+    time: int
+        delay in ms
+    dirr: bool or 1 or 0
+        Dirrection of step. Left or right rotation.
+    '''
+    for z in range(0, steps):
+        step(time, dirr)
+    for i in range(0, 4):
+        m_pins[i].off()
+
 
 def smootstep_l(time):
+    '''Rotate 1 full halvestep to left.
+
+    Parameters
+    ----------
+    sleep: int
+        delay in ms
+    '''
     for i in range(4):
-        m_pins[(0+i)%4].on(); m_pins[(1+i)%4].off(); m_pins[(2+i)%4].off(); m_pins[(3+i)%4].off()
+        m_pins[(0+i) % 4].on()
+        m_pins[(1+i) % 4].off()
+        m_pins[(2+i) % 4].off()
+        m_pins[(3+i) % 4].off()
         sleep_ms(time)
-        m_pins[(0+i)%4].on(); m_pins[(1+i)%4].on(); m_pins[(2+i)%4].off(); m_pins[(3+i)%4].off()
-        sleep_ms(time)
-        
-def smootstep_r(time):
-    for i in range(4):
-        m_pins[(0-i)%4].on(); m_pins[(1-i)%4].off(); m_pins[(2-i)%4].off(); m_pins[(3-i)%4].off()
-        sleep_ms(time)
-        m_pins[(0-i)%4].on(); m_pins[(1-i)%4].off(); m_pins[(2-i)%4].off(); m_pins[(3-i)%4].on()
+        m_pins[(0+i) % 4].on()
+        m_pins[(1+i) % 4].on()
+        m_pins[(2+i) % 4].off()
+        m_pins[(3+i) % 4].off()
         sleep_ms(time)
 
+
+def smootstep_r(time):
+    '''Rotate 1 full halvestep to right.
+
+    Parameters
+    ----------
+    sleep: int
+        delay in ms
+    '''
+    for i in range(4):
+        m_pins[(0-i) % 4].on()
+        m_pins[(1-i) % 4].off()
+        m_pins[(2-i) % 4].off()
+        m_pins[(3-i) % 4].off()
+        sleep_ms(time)
+        m_pins[(0-i) % 4].on()
+        m_pins[(1-i) % 4].off()
+        m_pins[(2-i) % 4].off()
+        m_pins[(3-i) % 4].on()
+        sleep_ms(time)
+
+
 def smootsteps(steps, time, dirr='left'):
+    '''Rotate multiple steps.
+
+    Parameters
+    ----------
+    steps: int
+        number of steps
+    time: int
+        delay in ms
+    dirr: str: 'left' or 'right'
+        Dirrection of step. Left or right rotation.
+    '''
     if dirr == 'left':
         for i in range(0, steps):
             smootstep_l(time)
@@ -39,23 +110,34 @@ def smootsteps(steps, time, dirr='left'):
     for i in range(0, 4):
         m_pins[i].off()
 
-def steps(steps, time, dirr=0):
-    for z in range(0, steps):
-        step(time, dirr)
-    for i in range(0, 4):
-        m_pins[i].off()
 
 def ruitenwissers():
+    '''Rotate 10 times 100 steps left and 100 steps right.'''
     for i in range(10):
         smootsteps(100, 1, 'left')
         smootsteps(100, 1, 'right')
 
+
 def rotatedegree(degrees=45, time=1, *args):
-    if degrees < 0 :
+    '''Rotate degrees. 
+
+    Ratate degrees if negative degree the motor wil turn left,
+    positive degree motor turn right.
+
+    Parameters
+    ----------
+    time:int
+        delay in ms between the steps
+    degrees: float
+        rotate x degrees.
+    '''
+    if degrees < 0:
         dirr = 'left'
     else:
         dirr = 'right'
     smootsteps(abs(512/360*degrees), time=time, dirr=dirr)
 
+
 def rotatedegree_t(t, degrees=45):
-    return rotatedegree(degrees=45)
+    '''Function for machine.Timer interupt'''
+    return rotatedegree(degrees)
